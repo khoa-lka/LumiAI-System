0f4d81874438c8c46bfb193def57b6f944e1ed6a
Newbiechoicode <ngbaohuy206@gmail.com>
2026-06-12 10:25:24 +0700

    Update AccountRepository.java

--- src/main/java/com/cinema/backend/repositories/AccountRepository.java
+++ src/main/java/com/cinema/backend/repositories/AccountRepository.java
@@ -6,6 +6,11 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 @Repository
-public interface AccountRepository extends JpaRepository<Account, Long> {
-    Optional<Account> findByEmailOrPhoneNumber(String email, String phoneNumber);
+public interface AccountRepository extends JpaRepository<Account, Integer> {
+    // Tìm tài khoản bằng email hoặc số điện thoại khi đăng nhập
+    Optional<Account> findByEmailOrPhone(String email, String phone);
+    
+    // Kiểm tra trùng lặp khi đăng ký thành viên mới
+    boolean existsByEmail(String email);
+    boolean existsByPhone(String phone);
 }
