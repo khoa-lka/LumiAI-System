ea0b6a7ba45fa72c165422d88529be19516dd171
Văn Viên <vanvien.fptu@gmail.com>
2026-06-24 23:37:59 +0700

    Update giao dien cuc mang

--- src/main/java/com/cinema/backend/repositories/PromotionRepository.java
+++ src/main/java/com/cinema/backend/repositories/PromotionRepository.java
@@ -9,6 +9,6 @@ import com.cinema.backend.entities.Promotion;
 public interface PromotionRepository extends JpaRepository<Promotion, Long> {
     // JpaRepository cung cấp sẵn các phương thức CRUD cơ bản như save(), findById(), findAll(), deleteById()...
     // Bạn có thể thêm các phương thức tùy chỉnh nếu cần, ví dụ:
-    // List<Promotion> findByStatus(String status);
+    // List<Promotion> findByStatus(String status); chaihfiahfafihafugbaueyifgaeieyufvauef
     List<Promotion> findByStatus(String status);
 }
